9cc1086277ddc9cdc64757bd5e317672b16925a7
Prasoon Saxena <mail.prasoonsaxena@gmail.com>
2025-08-20 19:05:51 +0530

    LEARNING

--- vastu_all_in_one.py
+++ vastu_all_in_one.py
@@ -2636,14 +2636,16 @@ class GenerateView:
             self._draw()
             return
         bed_plan=GridPlan(self.bed_Wm,self.bed_Hm)
+       
         if self.bath_dims and self.bath_openings:
             bwall, bstart, bwidth = self.bath_openings.door_span_cells()
             if bwall == 3:  # left/shared wall
+                # Use the same depth as the bathroom door swing
+                depth = bed_plan.meters_to_cells(self.bath_openings.swing_depth) + max(1, PATH_WIDTH_CELLS - 1)
                 pw = max(1, PATH_WIDTH_CELLS)
-                bed_plan.mark_clear(
-                    pw, bstart, pw, bwidth,
-                    'DOOR_CLEAR', 'BATHROOM_DOOR'
-                )
+                # Mark clearance just inside the bedroom, adjacent to the shared wall
+                bed_plan.mark_clear(depth, bstart, pw, bwidth, 'DOOR_CLEAR', 'BATHROOM_DOOR')       
+                
         solver=BedroomSolver(
             bed_plan,
             self.bed_openings,
