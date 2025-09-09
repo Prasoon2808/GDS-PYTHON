6f6c9cf5b6a52deb22b7d226667966dcb3fba2c6
PRASOON SAXENA <mail.prasoonsaxena@gmail.com>
2025-09-09 18:43:53 +0530

    Add kitchen planning support

--- tests/test_combine_plans.py
+++ tests/test_combine_plans.py
@@ -11,11 +11,13 @@ def test_combine_plans_handles_offsets_without_index_error():
     gv.bed_plan = GridPlan(side, side)
     gv.bath_plan = GridPlan(side, side)
     gv.liv_plan = GridPlan(side, side)
+    gv.kitch_plan = GridPlan(side, side)
 
     # Place items at the furthest cells to ensure indexing occurs at offsets
     gv.bed_plan.place(gv.bed_plan.gw - 1, 0, 1, 1, "BED")
     gv.bath_plan.place(gv.bath_plan.gw - 1, 0, 1, 1, "WC")
     gv.liv_plan.place(0, gv.liv_plan.gh - 1, 1, 1, "SOFA")
+    gv.kitch_plan.place(gv.kitch_plan.gw - 1, gv.kitch_plan.gh - 1, 1, 1, "SINK")
 
     try:
         GenerateView._combine_plans(gv)
